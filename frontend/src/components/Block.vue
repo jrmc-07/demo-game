f29e22f6b7a268321514fe08b0e5cc414aaee6d5
Cornejo <raphael.cornejo@towerswatson.com>
2019-02-11 17:59:14 +0800

    Added Block component

---
+++ frontend/src/components/Block.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="block"> {{number}}
+        <div v-if="player" :class="stateClass">
+            <p v-text="player.name"></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Block',
+    props: {
+        player: {
+            type: Object,
+            required: false,
+        },
+        number: {
+            type: Number,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            //
+        }
+    },
+    computed: {
+        stateClass() {
+            switch(this.player.state) {
+                case 0:
+                    return;
+                case 1:
+                    return "boxed";
+                case 2:
+                    return "boxed colored";
+                default:
+                    return;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.block {
+    height: 70px;
+    width: 70px;
+}
+.boxed {
+    border: 1px;
+    border-style: solid;
+    border-color: black;
+}
+.colored {
+    background-color: red;
+}
+</style>
+
