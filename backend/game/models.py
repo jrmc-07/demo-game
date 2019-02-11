f5bb901d587b679df7f437aa69a1648ff4397e17
Rap Cornejo <Raphael.Cornejo@willistowerswatson.com>
2019-02-11 21:22:32 +0800

    Added Player model

---
+++ backend/game/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Player(models.Model):
+    LEVEL_CHOICES = (
+        ('RE', 'Regular'),
+        ('BX', 'Boxed'),
+        ('CO', 'Colored'),
+    )
+    name = models.CharField(max_length=25)
+    position = models.IntegerField()
+    color = models.CharField(max_length=25)
+    level = models.CharField(
+        max_length=2,
+        choices=LEVEL_CHOICES,
+        default='RE',
+    )
