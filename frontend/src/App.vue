09c8bcaace8642d4f5fec7c4f04e3e33eb970809
Cornejo <raphael.cornejo@towerswatson.com>
2019-02-11 18:01:45 +0800

    Added views directory; added DemoGame view; Updated App vue to display DemoGame

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <demo-game />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './views/HelloWorld.vue'
+import DemoGame from './views/DemoGame.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    DemoGame,
   }
 }
 </script>
